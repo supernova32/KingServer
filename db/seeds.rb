2e450f0b2775c2abb7fad5359f2fa2b7c6532817
Patricio Cano <suprnova32@gmail.com>
2013-04-22 12:50:12 +0200

    Added seeds for all study_courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-informatik = StudyCourse.create [name: 'Informatik']
+StudyCourse.create [name: 'Architektur']
+StudyCourse.create [name: 'Innenarchitektur']
+StudyCourse.create [name: 'KlimaEngineering']
+StudyCourse.create [name: 'Bauingenieurwesen']
+StudyCourse.create [name: 'Infrastrukturmanagement']
+StudyCourse.create [name: 'Wirtschaftsingenieurwesen Bau und Immobilien']
+StudyCourse.create [name: 'Bauphysik']
+StudyCourse.create [name: 'KlimaEngineering']
+StudyCourse.create [name: 'Informatik']
+StudyCourse.create [name: 'Informationslogistik']
+StudyCourse.create [name: 'Vermessung und Geoinformatik']
+StudyCourse.create [name: 'Wirtschaftsinformatik']
+StudyCourse.create [name: 'Mathematik']
+StudyCourse.create [name: 'Betriebswirtschaft']
+StudyCourse.create [name: 'Wirtschaftsingenieurwesen Bau und Immobilien']
+StudyCourse.create [name: 'Wirtschaftspsychologie']
+
 bau_1 = Building.create [name: 'My Home', latitude: 48.7663727, longitude: 9.1780033]
 
 admin = User.create [name: 'Patricio Cano', email: 'pato.cano@me.com', password: 'adminPassword',
